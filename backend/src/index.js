eea9cfb87dc077205af628898172dc21f537a523
ShkarupaDC <dimon.shkarupa@gmail.com>
2021-03-07 20:23:36 +0200

    Add pre-commit git hook

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,8 +4,8 @@ const http = require('http');
 const { HOST, PORT } = process.env;
 
 const server = http.createServer((req, res) => {
-	res.end('Hello from DevSays\n');
+  res.end('Hello from DevSays\n');
 });
 server.listen(PORT, HOST, () => {
-	console.dir(`Server is running on http://${HOST}:${PORT}/`);
+  console.dir(`Server is running on http://${HOST}:${PORT}/`);
 });
