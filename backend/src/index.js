c1514083c11a943d332a4ba65dec74aa70cc792d
Vadym Kinchur <vkincur@gmail.com>
2021-03-27 11:10:53 +0200

    Fix workflow to work with buildx

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,10 +5,6 @@ const path = require('path');
 
 module.exports = async function (fastify, opts) {
   fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'plugins'),
-    options: Object.assign({}, opts),
-  });
-  fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'services'),
     options: Object.assign({}, opts),
   });
