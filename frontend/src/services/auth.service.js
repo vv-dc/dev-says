81c95ff941237ce98be2ffed6bbb56aee1705c50
Vadym Kinchur <vkincur@gmail.com>
2021-04-04 15:21:25 +0300

    Implement Http and Auth services, replace provider with Auth store

--- frontend/src/services/auth.service.js
+++ frontend/src/services/auth.service.js
@@ -1,12 +1,59 @@
-const tokenMockup = {
-  accessToken: 'sometokenhere',
-};
+import AuthStore from '../stores/auth-store';
+import { HttpService } from './http.service';
+import { getFingerprint } from '../helpers/get-fingerprint';
 
-export async function refreshToken() {
-  return Promise.resolve(tokenMockup);
-}
+const http = new HttpService({ withAuth: false });
 
-// eslint-disable-next-line no-unused-vars
-export async function loginUser(login, password) {
-  return Promise.resolve(tokenMockup);
+export class AuthService {
+  static async login(login, password) {
+    const fingerprint = await getFingerprint();
+    const response = await http.post(
+      '/auth/login',
+      { login, password, fingerprint },
+      { withCredentials: true }
+    );
+    AuthStore.setAuthData(response.data);
+  }
+
+  static async register(email, username, password) {
+    await http.post('/auth/register', { email, username, password });
+  }
+
+  static async logout() {
+    await new HttpService({ withAuth: true }).post(
+      '/auth/logout',
+      {},
+      { withCredentials: true }
+    );
+    AuthStore.resetAuthData();
+  }
+
+  static async refreshTokens() {
+    try {
+      const fingerprint = await getFingerprint();
+      const response = await http.post(
+        '/auth/refresh-tokens',
+        { fingerprint },
+        { withCredentials: true }
+      );
+      AuthStore.setAuthData(response.data);
+    } catch (error) {
+      AuthStore.resetAuthData();
+      return error;
+    }
+  }
+
+  static isAccessTokenExpired() {
+    const expDate = AuthStore.expiredAt - 10;
+    const nowDate = Math.floor(new Date().getTime() / 1000);
+    return expDate <= nowDate;
+  }
+
+  static isAuthenticated() {
+    return Boolean(AuthStore.accessToken);
+  }
+
+  static getAccessToken() {
+    return AuthStore.accessToken;
+  }
 }
