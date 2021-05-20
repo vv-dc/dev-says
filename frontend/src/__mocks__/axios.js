af3601359b00c3d4865d2e15bb939513cf84be0f
Vadym Kinchur <vkincur@gmail.com>
2021-05-20 17:36:06 +0300

    Add tests for auth.service on the frontend

---
+++ frontend/src/__mocks__/axios.js
@@ -0,0 +1,4 @@
+export default {
+  get: jest.fn(),
+  post: jest.fn(),
+};
---
+++ frontend/src/services/__mocks__/http.service.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+export class HttpService {
+  // eslint-disable-next-line no-unused-vars
+  constructor({ withAuth } = false) {
+    return axios;
+  }
+}
